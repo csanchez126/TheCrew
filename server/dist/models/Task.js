678912a606168d15a7218cc467a76182d8e1cfd2
Carlos Sanchez <csanchedz126@gmail.com>
2020-09-26 13:23:03 -0400

    Clean up dist folder from repo

--- server/dist/models/Task.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Task = void 0;
-const Card_1 = require("./Card");
-class Task extends Card_1.Card {
-}
-exports.Task = Task;
-//# sourceMappingURL=Task.js.map
